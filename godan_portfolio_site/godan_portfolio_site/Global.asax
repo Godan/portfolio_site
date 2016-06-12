20e595f8013e300a7d2cd7a2139b30d976c25bca
godan <shogo070920@gmail.com>
2016-06-12 22:53:25 +0900

    初期ファイル追加

---
+++ godan_portfolio_site/godan_portfolio_site/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="godan_portfolio_site.MvcApplication" Language="C#" %>
---
+++ godan_portfolio_site/godan_portfolio_site/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(godan_portfolio_site.Startup))]
+namespace godan_portfolio_site
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
